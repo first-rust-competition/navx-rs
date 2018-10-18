02e662ba91578d756712d738f6e77d2d18e43c0c
Josh Hejna <josh.hejna@gmail.com>
2018-10-18 13:55:37 -0700

    Relicense under dual MIT/Apache2.0

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Rust support for frc NavX build on top of [wpilib](https://github.com/Lytigas/first-rust-competition).
 
 Currently, only the SPI interface and get_yaw are implemented. The framework is in place for all the other register based protocols, though. PRs welcome!
+
+## License
+
+The contents of this repository are distributed under the terms of both the
+MIT license and the Apache License (Version 2.0). By contributing, you agree
+to license your contribution under these terms.
+
+See [LICENSE-APACHE](LICENSE-APACHE), [LICENSE-MIT](LICENSE-MIT), for details.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-// This file is part of "navx-rs", which is free software: you
-// can redistribute it and/or modify it under the terms of the GNU General
-// Public License version 3 as published by the Free Software Foundation. See
-// <https://www.gnu.org/licenses/> for a copy.
+// Copyright 2018 navx-rs Developers.
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
 #![allow(dead_code)]
 extern crate byteorder;
